7e5c0e8874e8ddf98f9a8c179cbe0531ceb2f95c
GAEL DE ROUSSET RASPANTINI SALVADOR <24018834870@info.local>
2026-04-24 11:01:36 -0300

    Adicionado secrets.example

---
+++ include/secrets.example.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_EXEMPLE_H
+#define SECRETS_EXEMPLE_H
+//Wifi config
+const char* WIFI_SSID = ""; // Aqui fica o nome da rede
+const char* WIFI_SENHA = ""; //Aqui fica a senha da rede
+
+
+
+#endif
--- include/secrets.h
+++ include/secrets.h
@@ -1,5 +1,5 @@
 #ifndef SECRETS_H
-#define SECRETS_H
+#define SECRETS__H
 //Wifi config
 const char* WIFI_SSID = "SALA 09";
 const char* WIFI_SENHA = "info@134";
